de51abd118563016668449f573d35e27a9d815f5
ZyyyL-Dev <69803117+zylrey@users.noreply.github.com>
2024-04-30 16:36:10 +0800

    Upload files
    This file is used to calculate tax in growtopia casino game (Black Jack)

---
+++ HUGE tax calculator/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace HUGE_tax_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBet = new System.Windows.Forms.TextBox();
+            this.cbPlayers = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.lblPot = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblTax = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Players";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(15, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total Bet";
+            // 
+            // txtBet
+            // 
+            this.txtBet.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBet.Location = new System.Drawing.Point(140, 194);
+            this.txtBet.Name = "txtBet";
+            this.txtBet.Size = new System.Drawing.Size(183, 33);
+            this.txtBet.TabIndex = 3;
+            this.txtBet.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtBet_KeyPress_1);
+            // 
+            // cbPlayers
+            // 
+            this.cbPlayers.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPlayers.FormattingEnabled = true;
+            this.cbPlayers.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4"});
+            this.cbPlayers.Location = new System.Drawing.Point(141, 145);
+            this.cbPlayers.Name = "cbPlayers";
+            this.cbPlayers.Size = new System.Drawing.Size(183, 33);
+            this.cbPlayers.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 428);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(271, 231);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(52, 15);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "10% TAX";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(19, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(304, 49);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.Location = new System.Drawing.Point(32, 261);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(129, 25);
+            this.lblWinner.TabIndex = 9;
+            this.lblWinner.Text = "Winner\'s Win:";
+            // 
+            // lblWin
+            // 
+            this.lblWin.AutoSize = true;
+            this.lblWin.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWin.Location = new System.Drawing.Point(191, 261);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(23, 25);
+            this.lblWin.TabIndex = 10;
+            this.lblWin.Text = "0";
+            // 
+            // lblPot
+            // 
+            this.lblPot.AutoSize = true;
+            this.lblPot.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPot.Location = new System.Drawing.Point(190, 318);
+            this.lblPot.Name = "lblPot";
+            this.lblPot.Size = new System.Drawing.Size(23, 25);
+            this.lblPot.TabIndex = 13;
+            this.lblPot.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(32, 369);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 25);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Total Tax:";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(19, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(304, 49);
+            this.button3.TabIndex = 14;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(131, 428);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(93, 35);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click_1);
+            // 
+            // lblTax
+            // 
+            this.lblTax.AutoSize = true;
+            this.lblTax.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTax.Location = new System.Drawing.Point(190, 369);
+            this.lblTax.Name = "lblTax";
+            this.lblTax.Size = new System.Drawing.Size(23, 25);
+            this.lblTax.TabIndex = 16;
+            this.lblTax.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(19, 360);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(304, 49);
+            this.button4.TabIndex = 17;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(32, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 25);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Total Pot:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(344, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(342, 472);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblTax);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblPot);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbPlayers);
+            this.Controls.Add(this.txtBet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Huge Tax Calculator By ZyyyL";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBet;
+        private System.Windows.Forms.ComboBox cbPlayers;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Label lblWin;
+        private System.Windows.Forms.Label lblPot;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblTax;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HUGE tax calculator/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HUGE_tax_calculator
+{
+    public partial class Form1 : Form
+    {
+        // Tax rate in decimal (10% = 0.10)
+        private const decimal taxRate = 0.10m;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            // Set default number of players (can be changed by the user)
+            cbPlayers.SelectedIndex = 0;
+
+            // Clear previous results and winner display
+            ClearResults();
+        }
+
+        private void ClearResults()
+        {
+            // Clear player bet TextBox, winner display, and pot label
+            txtBet.Text = "";
+            lblWin.Text = "";
+            lblPot.Text = "";
+            lblTax.Text = "";
+        }
+
+        private void CalculateWinner()
+        {
+            // Parse player bet from TextBox
+            if (decimal.TryParse(txtBet.Text, out decimal bet))
+            {
+                int numPlayers = int.Parse(cbPlayers.SelectedItem.ToString());
+
+                // Calculate the total pot (sum of all bets)
+                decimal pot = bet * numPlayers;
+                lblPot.Text = $"{Math.Round(pot, 0)} WLS";
+
+                // Calculate the total tax amount collected from all players
+                decimal totalTax = (numPlayers - 1) * (bet * taxRate);
+                lblTax.Text = $"{Math.Round(totalTax, 0)} WLS";
+
+                // Calculate the winner's result after applying tax to other players
+                decimal winnerResult = pot - totalTax;
+
+                lblWin.Text = $"{Math.Round(winnerResult, 0)} WLS";
+            }
+            else
+            {
+                MessageBox.Show("Invalid bet! Please enter a valid number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            CalculateWinner();
+        }
+
+        private void cbPlayers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ClearResults();
+        }
+
+        private void BtnClear_Click_1(object sender, EventArgs e)
+        {
+            ClearResults();
+        }
+
+        private void TxtBet_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            // Check if the pressed key is Enter
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                // Perform the calculation when Enter is pressed
+                CalculateWinner();
+
+                // Prevent further handling of the Enter key press event
+                e.Handled = true;
+            }
+        }
+    }
+}
